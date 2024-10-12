02cdffcafb562318ab239cfb75946286cd143d2c
Revisto <theRevisto@gmail.com>
2024-10-12 17:47:27 +0330

    update web build

--- server/game/index.service.worker.js
+++ server/game/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1728700600|12280742993";
+const CACHE_VERSION = "1728742605|3754073712";
 const CACHE_PREFIX = "Haft Sangs-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
